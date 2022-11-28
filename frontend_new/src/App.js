b8ba4dade6021d72f42c8b14b99bc0a9ff9f1ed2
James Bain <jamescbain@gmail.com>
2022-11-28 11:21:51 -0700

    add lolwut component and virtual routes

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import FourOhFour from "./components/FourOhFour";
 import KillerWhales from "./components/KillerWhales";
 import Info from "./components/Info";
+import LolWut from "./components/LolWut";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/killerwhales" element={<KillerWhales />} />
       <Route path="/info" element={<Info />} />
+      <Route path="/lolwut" element={<LolWut />} />
       <Route path="*" element={<FourOhFour />} />
     </Routes>
   );
---
+++ frontend_new/src/components/LolWut/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Wrapper, Content, Title } from "./styles";
+
+const LolWut = () => (
+  <Wrapper>
+    <Content>
+      <Title>LolWut</Title>
+    </Content>
+  </Wrapper>
+);
+
+export default LolWut;
