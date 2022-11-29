d4858b4b6fdd51887a3c91a3682d833fe62b628b
James Bain <jamescbain@gmail.com>
2022-11-29 14:37:54 -0700

    create a single url example

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -1,20 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home";
-import About from "./components/About";
 import FourOhFour from "./components/FourOhFour";
-import KillerWhales from "./components/KillerWhales";
-import Info from "./components/Info";
-import LolWut from "./components/LolWut";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/killerwhales" element={<KillerWhales />} />
-      <Route path="/info" element={<Info />} />
-      <Route path="/lolwut" element={<LolWut />} />
       <Route path="*" element={<FourOhFour />} />
     </Routes>
   );
